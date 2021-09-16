244b52e8d0642a8e576d5fca0cf7b0d1a0674a20
wintermute <kjulius@gmx.de>
2021-09-16 22:24:01 +0200

    fixed install script shebang again, goddamn

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 echo "Installing configured dotfiles to the system! Are you sure? [y/n]"
 
 read INPUT_STRING
-if INPUT_STRING == "y"; then
+if $INPUT_STRING == "y"; then
       cp ./nvim/ ~/.config/nvim/
 fi
