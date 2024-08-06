d46075717b7556f3b4a1798b2ad4251521e31ddb
wintermute <kjulius@gmx.de>
2024-08-06 17:08:21 +0200

    docs: add first install comments to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,34 @@
 {
+
+  # ===========================================================================
+  # On first install, this flake needs to run to install home-manager:
+  #     `nix run . -- build --flake .`
+  #
+  # Then activate the configuration with:
+  #     `nix run . -- switch --flake $HOME'/dotfiles?submodules=1';`
+  #
+  # Since the home-manager config should specify 
+  #     `programs.home-mnager.enable = true`
+  # this should make the `home-manager` command available.
+  #
+  # From then on, use this to activate the configuration:
+  #     `home-manager switch --flake $HOME'/dotfiles?submodules=1';`
+  #
+  # (instead of the dot as the flake path, a full path can be specified ofc)
+  # ===========================================================================
+
   description = "home manager flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur.url = "github:nix-community/NUR";
   };
 
